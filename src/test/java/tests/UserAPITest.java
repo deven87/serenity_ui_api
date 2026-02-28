676db0bd0c9bd0c37465fd986a0edcaec77a1ed9
deven87 <karayla.devendra@gmail.com>
2026-02-28 17:57:37 +0530

    added headless

--- src/test/java/tests/UserAPITest.java
+++ src/test/java/tests/UserAPITest.java
@@ -3,6 +3,7 @@ package tests;
 import api.steps.UserApiSteps;
 import net.serenitybdd.annotations.Steps;
 import net.serenitybdd.annotations.WithTag;
+import net.serenitybdd.annotations.WithTags;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,7 +14,10 @@ public class UserAPITest {
     @Steps
     UserApiSteps userApiSteps;
 
-    @WithTag("API")
+    @WithTags({
+            @WithTag("API"),
+            @WithTag("smoke")
+    })
     @Test
     public void getUserDetailsTest() {
         userApiSteps.getUserDetails(2);
